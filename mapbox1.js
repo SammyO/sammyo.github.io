4c64db250164a432d601562806da39c4ce2d9cf1
AndreHazes020 <99401031+AndreHazes020@users.noreply.github.com>
2022-02-10 16:21:06 +0100

    Update mapbox1.js

--- mapbox1.js
+++ mapbox1.js
@@ -4,7 +4,7 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoic2FtbXlvc3VtdGluZyIsImEiOiJja3llbHd2c2YwdDNuM
        container: 'map',
        style: 'mapbox://styles/sumting/ckzgwchr2000116lcvusqd79j',
        center: [44.594, 11.447],
-       zoom: 0.3,
+       zoom: 1,
        scrollZoom: true
    });
 
