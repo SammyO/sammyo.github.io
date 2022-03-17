5c1eac0bc56c02d325a601be2d8cf0b1f0ef8192
SammyO <sammy.odenhoven@gmail.com>
2022-03-17 09:36:41 +0000

    Update DivineBamboo1.js

--- DivineBamboo1.js
+++ DivineBamboo1.js
@@ -60,7 +60,7 @@ var options = {
                /* Assign the `marker` class to each marker for styling. */
                el.className = 'marker';
 
-               el.src = marker.properties.projectmarker;
+               el.src = marker.properties.marker;
 
 
                /**
